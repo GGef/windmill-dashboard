c9bde6b5baedec84c432fb3f04da091a3a662560
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2023-09-14 10:55:41 +0100

    add Details

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -365,11 +365,8 @@ function rowTable(item){
            <div class='w-1/3 truncate' >
            <button class='dropdownbtton'
                         type='button' data-item-id='${item.id}'>
-                        <svg class='dropdownbtton' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-more-vertical'>
-                          <circle cx='12' cy='12' r='1'></circle>
-                          <circle cx='12' cy='5' r='1'></circle>
-                          <circle cx='12' cy='19' r='1'></circle>
-                        </svg>
+                        <a href='index1.php?action=property&id=${item.id}'</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16"> <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/> <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/> </svg>
                       </button>
            </div>
 
